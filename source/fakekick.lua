0e7b8c605f7618741725b1507c3ca5a9d673068a
cuda_ <84263084+cudadotsln@users.noreply.github.com>
2025-06-05 04:39:41 -0700

    Create fakekick.lua

---
+++ source/fakekick.lua
@@ -0,0 +1,23 @@
+local Players = game:GetService("Players")
+
+local canDoFakeKick = true
+
+Players.LocalPlayer.Chatted:Connect(function(msg)
+    if msg == "/e stopthefake" then
+        canDoFakeKick = false
+    end
+end)
+
+Players.PlayerRemoving:Connect(function(player)
+    function chatMessage(str)
+        if canDoFakeKick then
+            str = tostring(str)
+            if game.TextChatService.ChatVersion ~= Enum.ChatVersion.LegacyChatService then
+                game.TextChatService.TextChannels.RBXGeneral:SendAsync(str)
+            else
+                game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(str, "All")
+            end
+        end
+    end
+    chatMessage(getgenv().fakeKick_prefix .. "kick " .. (getgenv().fakeKick_useDisplay and player.DisplayName or player.Name))
+end)
